cab8938552d2795dae465c20b91f45c2f96ffa63
Pecthgamon Sangnual <petchgamon.s@ku.th>
2023-10-21 16:14:15 +0700

    just add daisy ui and ui to know what we will dispaly **not finish** i will do tonight

--- src/main/java/ku/cs/sa/torrungruang/controller/HomeController.java
+++ src/main/java/ku/cs/sa/torrungruang/controller/HomeController.java
@@ -2,6 +2,7 @@ package ku.cs.sa.torrungruang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,7 +10,11 @@ public class HomeController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        model.addAttribute("greeting", "This is a home page idk what to do with it");
+        model.addAttribute("greeting", "Hello");
+        // ต้องคืนค่าเป็นชื่อไฟล์ html template โดยในเมธอดนี้ คืนค่าเป็น home.html
         return "home";
     }
+    
 }
+
+
